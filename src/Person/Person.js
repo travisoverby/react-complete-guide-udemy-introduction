3ba70fb0acde97468c002c97c26e8474447fd44a
Travis Overby <tek62441@adobe.com>
2019-05-31 14:36:20 -0700

    Basics of React state management

---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const person = (props) => {
+	const name = props.name;
+	const age = props.age;
+	const children = props.children;
+
+	return (
+		<div>
+			<p>I'm {name} and I am <b>{age}</b> years old!</p>
+			<p>{children}</p>
+		</div>
+		);
+};
+
+export default person;
