57da760d9fc9b5d1d944a511ebef3e50199b3c32
Travis Overby <travisoverby@gmail.com>
2019-06-03 19:49:52 -0700

    Complete styling section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 import Person from './Person/Person';
 
@@ -60,20 +60,9 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-        backgroundColor: 'green',
-        color: 'white',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '8px',
-        cursor: 'pointer',
-        ':hover': {
-          backgroundColor: 'lightgreen',
-          color: 'black'
-        }
-    };
 
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -91,35 +80,30 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-          backgroundColor: 'salmon',
-          color: 'black'
-        }
+      btnClass = classes.Red;
+
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={classes}>This is really working!</p>
           <button 
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonsHandler}>
             Toggle Persons
           </button>
           {persons}
         </div>
-
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
 	const name = props.name;
@@ -8,7 +8,7 @@ const person = (props) => {
 	const children = props.children;
 
 	return (
-		<div className="Person">
+		<div className={classes.Person}>
 			<p onClick={props.click}>I'm {name} and I am <b>{age}</b> years old!</p>
 			<p>{children}</p>
 			<input type="text" onChange={props.changed} value={name}/>
