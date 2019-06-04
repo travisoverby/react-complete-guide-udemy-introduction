0135bf71742a1789730f25747cbafa227c7a68d7
Travis Overby <tek62441@adobe.com>
2019-06-04 10:06:05 -0700

    Remove 'ErrorBoundary' code from application

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import classes from './App.css';
 
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -71,13 +70,12 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
                 <Person 
                   click={() => this.deletePersonHandler(index)}
                   name={person.name} 
                   age={person.age} 
-                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
-              </ErrorBoundary>
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
+                  key={person.id} />
             );
           })}
         </div>
