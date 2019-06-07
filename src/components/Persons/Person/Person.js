edc9394888a9649a911470612cf28d2662259d99
Travis Overby <tek62441@adobe.com>
2019-06-07 16:21:01 -0700

    Complete 'React components and internals deep dive' module

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,48 +7,48 @@ import AuthContext from '../../../context/auth-context';
 import classes from './Person.css';
 
 class Person extends Component {
-	constructor(props) {
-		super(props);
-		this.inputElementRef = React.createRef();
-	}
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
 
-	static contextType = AuthContext;
+  static contextType = AuthContext;
 
-	componentDidMount() {
-		// this.inputElement.focus();
-		// this.inputElementRef.current.focus();
-		this.inputElementRef.current.focus();
-		console.log(this.context.authenticated);
-	}
+  componentDidMount() {
+    // this.inputElement.focus();
+    // this.inputElementRef.current.focus();
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
 
-	render() {
-		console.log('[Person.js] rendering...');
-		return (
-			<Aux>
-				{this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
-				<p key="i1"
-					onClick={this.props.click} >
-					I'm {this.props.name} and I am <b>{this.props.age}</b> years old!
-				</p>
-				<p key="i2">
-					{this.props.children}
-				</p>
-				<input 
-					key="i3"
-					ref={this.inputElementRef}
-					type="text"
-					onChange={this.props.changed} 
-					value={this.props.name} />
-			</Aux>
-		);
-	}
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      <Aux>
+        {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+        <p key="i1"
+          onClick={this.props.click} >
+          I'm {this.props.name} and I am <b>{this.props.age}</b> years old!
+        </p>
+        <p key="i2">
+          {this.props.children}
+        </p>
+        <input 
+          key="i3"
+          ref={this.inputElementRef}
+          type="text"
+          onChange={this.props.changed} 
+          value={this.props.name} />
+      </Aux>
+    );
+  }
 }
 
 Person.propTypes = {
-	click: PropTypes.func,
-	name: PropTypes.string,
-	age: PropTypes.number,
-	changed: PropTypes.func
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const authContext = React.createContext({
-	authenticated: false, 
-	login: () => {},
+  authenticated: false, 
+  login: () => {},
 });
 
 export default authContext;
